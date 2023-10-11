821fc98f2029caa98f2bb96d41b32a640aa87bce
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-10-11 10:16:09 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  colores: string[] = ["rojo", "azul","amarillo", "verde", "naranja", "rosa", "blanco", "negro", "morado", "marrón"]
+  colorSecreto  : string = "";
+  colorUsuario : string = "";
+  txtMensaje: string = "";
+  fallos: string[] = ["Prueba otro color"," El color no es el correcto", "Inténtalo de nuevo", "Prueba otra vez", "No has acertado"];
+  esCorrecto: boolean = false;
+
+  constructor() {
+    this.generarColorSecreto();
+  }
+
+  comprobarColor(){
+    console.log("click en el botón");
+    console.log("ColorUsuario=" + this.colorUsuario );
+    if(this.colorUsuario  == this.colorSecreto){
+      console.log("Ha acertado");
+      this.txtMensaje = "¡Ha acertado!";
+      this.esCorrecto = true;
+    } else {
+      console.log("El color no es el correcto");
+      this.txtMensaje = this.fallos[Math.floor(Math.random() * this.fallos.length)];;
+    }
+  }
+
+  generarColorSecreto(){
+    this.colorSecreto = this.colores[Math.floor(Math.random() * this.colores.length)];
+    console.log("colorSecreto =" + this.colorSecreto);
+  }
+
+  reiniciar(){
+    this.generarColorSecreto();
+    this.esCorrecto = false;
+    this.colorUsuario  = "";
+    this.txtMensaje = "";
+  }
 
 }
